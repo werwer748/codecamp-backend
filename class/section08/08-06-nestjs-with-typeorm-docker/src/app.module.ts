efe5c1747423321b93ed30b2fb68aaeb5e848c3e
hugo <werwer748@naver.com>
2023-10-27 11:24:59 +0900

    nest기본기
    쉽지 않아

--- class/section08/08-06-nestjs-with-typeorm-docker/src/app.module.ts
+++ class/section08/08-06-nestjs-with-typeorm-docker/src/app.module.ts
@@ -4,23 +4,25 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from 'src/apis/boards/boards.module';
 import { Board } from 'src/apis/boards/entities/board.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     BoardsModule,
     // ProductsModule,
     // UsersModule,
+    ConfigModule.forRoot(), // process.env보다 먼저 실행되어야 함
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'hugosnodejs',
-      database: 'myproject',
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
       entities: [Board],
       synchronize: true,
       logging: true, // sql문을 보여줌
