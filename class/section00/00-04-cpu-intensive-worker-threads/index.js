9140c6ac3afa3e0f8a69216d4db5175887a793bf
hugo <werwer748@naver.com>
2023-12-02 23:11:35 +0900

    section00

---
+++ class/section00/00-04-cpu-intensive-worker-threads/index.js
@@ -0,0 +1,16 @@
+const { Worker } = require("worker_threads");
+
+const start = () => {
+  let totalSum = 0;
+
+  for (let i = 0; i < 90; i++) {
+    const worker = new Worker("./worker.js");
+    worker.postMessage(1000000000);
+    worker.on("message", (result) => {
+      totalSum += result;
+      console.log(`나는 ${i}번째 일꾼이고, 현재까지 총 합은 ${totalSum}이다!`);
+    });
+  }
+};
+
+start();
